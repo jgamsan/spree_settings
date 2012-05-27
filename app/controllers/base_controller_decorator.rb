93736cfd5ceff9d0161d5bafffe9361d392c0514
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-27 19:59:43 +0200

    Initial commit

---
+++ app/controllers/base_controller_decorator.rb
@@ -0,0 +1,11 @@
+Spree::BaseController.class_eval do
+  
+  before_filter :myapp_force_get_settings
+
+  def myapp_force_get_settings
+    if session[:idioma].nil?
+      redirect_to settings_new_path and return
+    end
+  end 
+  
+end
