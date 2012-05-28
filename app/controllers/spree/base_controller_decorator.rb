73ebbc9f1be26f7c383ca8909f682a63a9a74e8a
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-28 20:57:01 +0200

    Initial commit

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::BaseController.class_eval do
 
   def myapp_force_get_settings
     if session[:idioma].nil?
-      redirect_to spree.setting_index_url
+      redirect_to spree.settings_index_url
     end
   end 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.routes.prepend do
-  match "/setting_index" => 'settings#index', :as => :setting_index
+  get "settings/index"
+  # match "/setting_index" => 'settings#index', :as => :setting_index
   match "/setting_parameters/:id" => 'settings#set_parameter', :as => :setting_parameters
   # Add your extension routes here
 end
