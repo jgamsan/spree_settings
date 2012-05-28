5fda8ea99d084ee47e1cddb1658852ae85d1d4bd
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-28 20:52:58 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.prepend do
-  match "/setting_index/" => 'settings#index', :as => :setting_index
+  match "/setting_index" => 'settings#index', :as => :setting_index
   match "/setting_parameters/:id" => 'settings#set_parameter', :as => :setting_parameters
   # Add your extension routes here
 end
