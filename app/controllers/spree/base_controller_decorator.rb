1882dc39e926bb152dc110080cea4a8b858c1f09
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-29 19:39:40 +0200

    Initial commit

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -3,10 +3,7 @@ Spree::BaseController.class_eval do
   before_filter :myapp_force_get_settings
 
   def myapp_force_get_settings
-    session[:idioma] = nil
-    if session[:idioma].nil?
-      redirect_to spree.settings_path
-    end
+    redirect_to settings_url
   end 
   
 end
