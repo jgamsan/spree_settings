3e249065b5bdd5fc37f52c08871ffbc50c14d780
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-05-29 13:32:31 +0200

    Cambios en routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Spree::Core::Engine.routes.prepend do
   
   get "settings/index"
    
-  match "/setting_parameters/:id" => 'settings#set_parameter', :as => :setting_parameters
+  match "/setting_parameters/:id" => 'spree/settings#set_parameter', :as => :setting_parameters
   # Add your extension routes here
 end
