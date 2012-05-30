54a4d04effbcebe17aa9dca5d6a2f973587a0c5a
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-30 18:46:22 +0200

    Initial commit

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -1,7 +1,9 @@
 class Spree::SettingsController < Spree::BaseController
-  layout 'initial'
+  
   def index
-    
+    respond_to do |format|
+      format.html { render :layout => 'initial'}
+    end
   end
 
   def set_parameter
