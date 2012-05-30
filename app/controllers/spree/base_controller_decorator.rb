23adf46c4460706fb704ffe2b1103af1da9c84eb
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-30 19:27:40 +0200

    Initial commit

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::BaseController.class_eval do
   before_filter :myapp_force_get_settings
 
   def myapp_force_get_settings
-    redirect_to products_url and return
+    redirect_to new_user_session_url and return 
   end 
   
 end
