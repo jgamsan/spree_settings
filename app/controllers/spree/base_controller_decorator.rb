536687339e08848bc18f91f2ca1b3aa3a043c859
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-30 19:40:18 +0200

    Initial commit

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::BaseController.class_eval do
   before_filter :myapp_force_get_settings
 
   def myapp_force_get_settings
-    redirect_to spree.set_parameter_setting_url and return 
+    redirect_to set_parameter_setting_url and return 
   end 
   
 end
