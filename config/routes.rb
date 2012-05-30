ac9bca607bb36880344a9ac34a7cdab6832b81ef
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-05-30 18:37:16 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Spree::Core::Engine.routes.draw do
   
   resources :settings,
     :controller => 'settings',
-    :only => [:index]
+    :only => [:index] do
+    member do
+      get 'set_parameter'
+    end
+  end
    
-  match "/setting_parameters/:id" => 'settings#set_parameter', :as => :setting_parameters
+  #match "/set_parameter/:id" => 'settings#set_parameter', :as => :setting_parameters
   # Add your extension routes here
 end
