721f525fc66e430ad7abe0853d80abc16f29071b
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-05-31 23:08:28 +0200

    Cambios en routing

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,7 +1,8 @@
 Spree::BaseController.class_eval do
   
-  #before_filter :myapp_force_get_settings
+  before_filter :myapp_force_get_settings
 
+  protected
   def myapp_force_get_settings
     if session[:idioma].nil?
       redirect_to spree.products_url
