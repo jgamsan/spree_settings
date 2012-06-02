e180d79214f2d783eee24d54f78bc3c125b66f29
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-02 20:11:08 +0200

    Initial commit

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -11,6 +11,6 @@ class Spree::SettingsController < Spree::BaseController
   def set_flag
     session[:locale] = params[:id]
     session[:vat] = 1.18
-    redirect_to('/', :notice => t('spree.contact_us.notices.success'))
+    redirect_to('/', :notice => t('spree.settings.notices.success'))
   end
 end
