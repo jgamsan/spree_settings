2e6df830ab9dc0c9381b91eae90ce31268a6e6ac
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-02 08:11:27 +0200

    Initial commit

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -4,10 +4,7 @@ class Spree::SettingsController < Spree::BaseController
     @setting = Spree::Setting.new
   end
 
-  def set_parameter
-    session[:idioma] = params[:id]
-    respond_to do |format|
-      format.html { redirect_to spree.root_url }
-    end
+  def create
+    
   end
 end
---
+++ app/models/spree/setting.rb
@@ -0,0 +1,5 @@
+module Spree
+  class setting
+    attr_accessor :flag
+  end
+end
