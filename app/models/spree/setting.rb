4ed21a17afa4d9046c4a0c999ceb40eb0aede242
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-02 10:18:18 +0200

    Initial commit

--- app/models/spree/setting.rb
+++ app/models/spree/setting.rb
@@ -1,5 +1,6 @@
 module Spree
   class Setting
     attr_accessor :flag
+    FLAGS_LIST_1 = %w["Austria.png" "Belgium.png" "Bosnia and Herzegovina.png" "Czech Republic.png" "Denmark.png" "Spain.png" "Finland.png" "Germany.png"   ]
   end
 end
