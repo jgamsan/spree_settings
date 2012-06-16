1ea26c8d50042aacf79d62c2821e32885b5f2036
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-16 19:18:06 +0200

    Added Search from for tires

--- app/overrides/include_vat_in_product_price.rb
+++ app/overrides/include_vat_in_product_price.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/products",
                      :name => "include_vat_in_product_price",
                      :replace => %q{.price_selling},
-                     :text => "<span class="price selling" itemprop="price"><%= number_to_currency product.price * session[:vat] %></span>")
+                     :text => %q{<span class="price selling" itemprop="price">
+                     <%= number_to_currency product.price * session[:vat] %></span>})
