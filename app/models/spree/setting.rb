57ad9901946c8787f0ae8b294920bdd6cc5f2bc1
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-18 11:37:48 +0200

    Added Search from for tires

--- app/models/spree/setting.rb
+++ app/models/spree/setting.rb
@@ -8,7 +8,10 @@ module Spree
     FLAGS_LIST_2 = %w[Denmark.png Switzerland.png Liechtenstein.png CzechRepublic.png France.png Monaco.png UnitedKingdom.png Italy.png Slovenia.png]
     FLAGS_LIST_3 = %w[Sweden.png Ireland.png Poland.png Slovakia.png Hungary.png]
     FLAGS_LIST_4 = %w[Norway.png Finland.png Estonia.png latvia.png Lithuania.png BosniaHerzegovina.png Croatia.png Serbia.png Bulgaria.png Romania.png]
-  
+    
+    def persisted?
+      false
+    end
   
   end
 end
