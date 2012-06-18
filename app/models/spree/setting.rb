658f3158bbc8f6bbc3544e54c5a599f70d827517
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-18 19:10:28 +0200

    Added Search from for tires

--- app/models/spree/setting.rb
+++ app/models/spree/setting.rb
@@ -5,7 +5,7 @@ module Spree
     attr_accessor :flag, :ancho, :perfil, :llanta, :velocidad, :ic, :fr, :tttl 
     
     FLAGS_LIST = %w[Spain.png Portugal.png Germany.png Austria.png Belgium.png Nederland.png Luxembourg.png
-    Denmark.png Switzerland.png Liechtenstein.png CzechRepublic.png France.png Monaco.png UnitedKingdom.png Italy.png Slovenia.png,
+    Denmark.png Switzerland.png Liechtenstein.png CzechRepublic.png France.png Monaco.png UnitedKingdom.png Italy.png Slovenia.png
     Sweden.png Ireland.png Poland.png Slovakia.png Hungary.png
     Norway.png Finland.png Estonia.png latvia.png Lithuania.png BosniaHerzegovina.png Croatia.png Serbia.png Bulgaria.png Romania.png]
     
