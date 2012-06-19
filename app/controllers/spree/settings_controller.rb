67b8a45993ee6c0602c392065318eaddb865c246
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-19 19:34:16 +0200

    Added Search from for tires

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -6,7 +6,7 @@ class Spree::SettingsController < Spree::BaseController
 
   def create
     @valor = Spree::Setting.new(params[:setting])
-    
+    @prueba = params.select{|k,v| k =~ /.x/}
   end
   
   def set_flag
