73dfb007e63ea15c0dc4b766d91f4389f80e1228
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-20 12:23:01 +0200

    Added Search from for tires

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -15,7 +15,7 @@ class Spree::SettingsController < Spree::BaseController
       when "11"
         session[:locale] = "fr"
     end
-    @products = Spree::Product.where(:tire_speed_code_id => 5)
+    @products = Spree::Product.joins(:variants_including_master).where("spree_variants.tire_speed_code_id = ?", 7)
     #redirect_to("/products", :notice => t('spree.settings.notices.success'))
     render :partial => "spree/shared/products", :products => @products, :taxon => @taxon
   end
