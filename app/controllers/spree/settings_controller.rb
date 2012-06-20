fa8208c4988bc0a6bafd82f928f394cccc8382fa
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-20 10:35:43 +0200

    Added Search from for tires

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -7,7 +7,7 @@ class Spree::SettingsController < Spree::BaseController
   def create
     #@valor = Spree::Setting.new(params[:setting])
     prueba = params.select{|k,v| k =~ /.x/}
-    case prueba.values.first
+    case prueba.keys.first
       when "0"
         session[:locale] = "es"
       when "1"
