69505f068c9d4c124a8719c81ebdb3d32e939e3e
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-21 21:56:52 +0200

    modified create action

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -29,6 +29,22 @@ class Spree::SettingsController < Spree::BaseController
     render "spree/home/index", :products => @products, :taxon => @taxon 
   end
   
+  def index
+    @velocidad = params[:setting][:velocidad]
+    prueba = params.select{|k,v| k =~ /.x/}
+    case prueba.keys.first.chop.chop
+      when "0"
+        session[:locale] = "es"
+      when "1"
+        session[:locale] = "en"
+      when "11"
+        session[:locale] = "fr"
+    end
+    @searcher = Spree::Config.searcher_class.new(:tire_speed_code_id => 7)
+    @products = @searcher.retrieve_products
+    render "spree/home/index", :products => @products, :taxon => @taxon
+  end
+  
   def set_flag
     session[:locale] = params[:id]
     session[:vat] = 1.18
