5cec2fe92ab811db5f14f142b67789067c7eb8d3
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-28 19:14:11 +0200

    Added flags locale

--- app/overrides/add_flag_locale.rb
+++ app/overrides/add_flag_locale.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => %q{spree/shared/_search},
                           :name => %q{add_flag_locale},
                           :insert_before => %q{code[erb-loud]:contains('form_tag')},
-                          :text => %q{<%= image_tag("flags_icons/#{session[:locale]}.gif") unless session[:locale].nil? %> })
+                          :text => %q{<%= image_tag("flag_icons/#{session[:locale]}.gif") unless session[:locale].nil? %> })
