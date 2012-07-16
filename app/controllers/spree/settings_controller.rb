6dbe7ba8b7711fce36f69b3d8edbd146ae897235
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-16 07:59:03 +0200

    Cambios with jquery in settings

--- app/controllers/spree/settings_controller.rb
+++ app/controllers/spree/settings_controller.rb
@@ -20,13 +20,13 @@ class Spree::SettingsController < Spree::BaseController
   
   def update_tires_select
     sql = ""
-    sql = sql + " and tire_width_id = #{params[:iw]}" unless params[:iw] == 0
-    sql = sql + " and tire_profile_id = #{params[:ip]}" unless params[:ip] == 0
-    sql = sql + " and tire_innertube_id = #{params[:ii]}" unless params[:ii] == 0
-    sql = sql + " and tire_ic_id = #{params[:ic]}" unless params[:ic] == 0
-    sql = sql + " and tire_speed_code_id = #{params[:is]}" unless params[:is] == 0
-    sql = sql + " and tire_fr_id = #{params[:if]}" unless params[:if] == 0
-    sql = sql + " and tire_tttl_id = #{params[:it]}" unless params[:it] == 0
+    sql = sql + " and tire_width_id = #{params[:iw]}" unless params[:iw] == "0"
+    sql = sql + " and tire_profile_id = #{params[:ip]}" unless params[:ip] == "0"
+    sql = sql + " and tire_innertube_id = #{params[:ii]}" unless params[:ii] == "0"
+    sql = sql + " and tire_ic_id = #{params[:ic]}" unless params[:ic] == "0"
+    sql = sql + " and tire_speed_code_id = #{params[:is]}" unless params[:is] == "0"
+    sql = sql + " and tire_fr_id = #{params[:if]}" unless params[:if] == "0"
+    sql = sql + " and tire_tttl_id = #{params[:it]}" unless params[:it] == "0"
     tires = Spree::Variant.find_by_sql("SELECT tire_width_id, tire_profile_id, tire_innertube_id, tire_ic_id, tire_speed_code_id, tire_fr_id, tire_tttl_id, is_master
      FROM spree_variants where is_master = 't'" + sql + " group by tire_width_id, tire_profile_id, tire_innertube_id, tire_ic_id, tire_speed_code_id, tire_fr_id, tire_tttl_id, is_master;")
     widths = tires.map {|x| x.tire_width_id}.flatten.uniq
