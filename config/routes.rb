2643d356018a76b6dfbda204a55cbd78a1439cfb
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-16 07:45:30 +0200

    Cambios with jquery in settings

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Spree::Core::Engine.routes.draw do
     :controller => 'settings',
     :only => [:new, :create, :index]
   match 'set-initial' => 'settings#new', :as => :set_initial
-  match '/settings/update_tires_select/:iw/:ip/:ii/:ic/:is/:if/:it', :to => 'settings/#update_tires_select'
+  match '/update_tires_select/:iw/:ip/:ii/:ic/:is/:if/:it', :to => 'settings/#update_tires_select'
 end
