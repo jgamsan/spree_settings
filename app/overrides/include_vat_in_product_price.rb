3f4a29b42047bdcad66dc1e2df30aa16484afc0b
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-17 16:21:35 +0200

    Cambios with jquery in settings

--- app/overrides/include_vat_in_product_price.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/products",
-                     :name => "include_vat_in_product_price",
-                     :replace => %q{.price_selling},
-                     :text => %q{<span class="price selling" itemprop="price">
-                     <%= number_to_currency product.price * session[:vat] %></span>})
